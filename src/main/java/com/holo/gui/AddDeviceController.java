299b162ec8afd58603f749b71c4d4c618d9734dd
Zach <18561222+zellman01@users.noreply.github.com>
2023-10-31 14:02:25 -0400

    #6 Fix device owner switching

--- src/main/java/com/holo/gui/AddDeviceController.java
+++ src/main/java/com/holo/gui/AddDeviceController.java
@@ -13,6 +13,7 @@ public class AddDeviceController {
     public static String deviceSerialDefault;
     public static String deviceMacDefault;
     public static String deviceNameDefault;
+    public static String deviceOwner;
     @FXML private TextField deviceSerial;
     @FXML private TextField deviceMac;
     @FXML private TextField deviceName;
@@ -67,6 +68,7 @@ public class AddDeviceController {
                    errorMessage.setText("Device enrollment failed");
                 }
             } else {
+                owner = deviceOwner;
                 ClientMain.getNetworkManager().send("UPDATE-DEVICE " + owner + " " + dSer + " " + dMac + " " + dNam);
                 if (ClientMain.getNetworkManager().parseServerMessage(ClientMain.getNetworkManager().recieve()).equals("DEVICE-UPDATE-YES")) {
                     ClientMain.setRoot("MainScreen");
